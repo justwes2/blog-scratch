f83e4c6e6f31bc3b6c8c125ae84332646c461fa3
Wes <wes.coffay@gmail.com>
2019-07-16 14:25:45 -0400

    updated configs with new analytics id, removed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-143420846-1",
+        trackingId: "UA-143420846-2",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
@@ -58,7 +58,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
