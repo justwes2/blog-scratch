f1f2b4595e06d29e40e2152221e8e769e41cbb86
Wes <wes.coffay@gmail.com>
2019-07-27 14:19:33 -0400

    added captions, proofreading

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
+              showCaptions: true,
             },
           },
         ],
