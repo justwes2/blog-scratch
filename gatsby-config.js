13e3c69bd86e836cacfdcc3a5b85c18e1e5057a2
Wes <wes.coffay@gmail.com>
2019-07-27 13:54:55 -0400

    added images to react

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,19 @@ module.exports = {
         path: `${__dirname}/src`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
